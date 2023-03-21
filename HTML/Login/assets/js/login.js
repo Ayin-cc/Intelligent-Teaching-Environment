36a01de7615ae4f6bf14feab28edea08fdbdf4d9
XingYueChenFu <128380223+XingYueChenFu@users.noreply.github.com>
2023-03-21 23:16:53 +0800

    Add files via upload

---
+++ HTML/Login/assets/js/login.js
@@ -0,0 +1,13 @@
+// 登录
+document.getElementById('login_btn').onclick = function () {
+    var input_username = document.getElementById("login_username").value;
+    var input_password = document.getElementById("login_password").value;
+    
+}
+// 注册
+document.getElementById('register_btn').onclick = function () {
+    var input_username = document.getElementById("register_username").value;
+    var input_password = document.getElementById("register_password").value;
+
+
+}
