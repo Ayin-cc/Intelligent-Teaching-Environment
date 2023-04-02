dd2c7f7b1aa836b8e7cf7f2a7aa6f6556c3200f5
XingYueChenFu <128380223+XingYueChenFu@users.noreply.github.com>
2023-04-02 15:10:11 +0800

    Add files via upload

--- HTML/administrator/html/information/message.js
+++ HTML/administrator/html/information/message.js
@@ -1,23 +1,46 @@
 $(document).ready(function () {
-  $('#submit-btn').click(function () {
-    var formData = {
-      "title": $('.input-title:first').text().trim(),
-      "content": $('.input-content:first').text().trim(),
-      "time": $('.input-time:first').text().trim()
-    };
-    var jsonData = JSON.stringify(formData);
-    $.ajax({
-      type: 'POST',
-      url: 'http://localhost:8080/SCUEE/JoinMsg',
-      data: jsonData,
-      contentType: 'application/json; charset=utf-8',
-      dataType: 'json',
-      success: function (data) {
-        alert('消息发送成功！');
-      },
-      error: function (xhr, status, error) {
-        alert('消息发送失败：' + error);
-      }
+    $('#submit-btn').click(function () {
+        var formData = {
+            "type": $('input[name="type"]:checked').val(),
+            "title": $('.input-title:first').text().trim(),
+            "content": $('.input-content:first').text().trim(),
+
+        };
+        var jsonData = JSON.stringify(formData);
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:8080/SCUEE/JoinMsg',
+            data: jsonData,
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (data) {
+                alert('消息发送成功！');
+            },
+            error: function (xhr, status, error) {
+                alert('消息发送失败：' + error);
+            }
+        });
     });
-  });
+    $('#scheduledSubmit-btn').click(function () {
+        var formData = {
+            "type": $('input[name="type"]:checked').val(),
+            "title": $('.input-title:first').text().trim(),
+            "content": $('.input-content:first').text().trim(),
+            "time": $('.input-time:first').text().trim()
+        };
+        var jsonData = JSON.stringify(formData);
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:8080/SCUEE/JoinMsg',
+            data: jsonData,
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (data) {
+                alert('消息上传成功！');
+            },
+            error: function (xhr, status, error) {
+                alert('消息上传失败：' + error);
+            }
+        });
+    });
 });
