39afcdeb134ab43efbb9fa7e15474b552620f2aa
XingYueChenFu <128380223+XingYueChenFu@users.noreply.github.com>
2023-05-28 17:19:17 +0800

    Add files via upload

--- Interactive-Interface/html/message/message.js
+++ Interactive-Interface/html/message/message.js
@@ -3,13 +3,11 @@ var message = document.getElementsByClassName("message-box")[0].getElementsByCla
 for(let i = 0 ; i < message.length; i++){
     message[i].addEventListener('click',function(){
         var message_content = message[i].getElementsByClassName('content')[0]
-        if(message_content.style.whiteSpace == 'nowrap'){
-            message_content.style.whiteSpace = 'normal'
-            message[i].classList.add('active');
-        }else{
+        if(message_content.style.whiteSpace == 'normal'){
             message_content.style.whiteSpace = 'nowrap'
-            message[i].classList.remove('active');
+        }else{
+            message_content.style.whiteSpace = 'normal'
         }
-        
+        message[i].classList.toggle('active');
     })
 }
