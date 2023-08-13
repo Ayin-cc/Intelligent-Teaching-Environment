b45d09f56259cdf422653d77c243188189d6cf2f
XingYueChenFu <128380223+XingYueChenFu@users.noreply.github.com>
2023-08-13 22:41:18 +0800

    Add files via upload

--- electron/src/index.js
+++ electron/src/index.js
@@ -1,6 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var clickTimeout;
 
+document.addEventListener("DOMContentLoaded", function () {
+    // 消除选择
+    var clickTimeout;
     document.addEventListener("click", function () {
         if (clickTimeout) {
             clearTimeout(clickTimeout);
@@ -11,6 +12,20 @@ document.addEventListener("DOMContentLoaded", function () {
                 var selection = window.getSelection();
                 selection.removeAllRanges();
             }
-        }, 3000); // 1000 milliseconds = 1 second
+        }, 3000); // 3s
+    });
+
+    // 检查主题模式
+    var checkbox = document.getElementById("theme-toggle");
+    var htmlElement = document.documentElement;
+    checkbox.addEventListener("change", function () {
+        var isChecked = checkbox.checked;
+        if (isChecked) {
+            console.log("已选中—darkMode");
+            htmlElement.classList.toggle("dark");
+        } else {
+            console.log("未选中—lightMode");
+            htmlElement.classList.toggle("dark");
+        }
     });
 });
