b1ef62c43a48865a8b9be1b2a008961973274058
XingYueChenFu <128380223+XingYueChenFu@users.noreply.github.com>
2023-08-21 02:04:55 +0800

    更新了签到界面（达到阶段性目标，但未完成）

--- electron/src/index.js
+++ electron/src/index.js
@@ -1,7 +1,7 @@
 
 // 当纯 HTML 被完全加载以及解析时
 document.addEventListener("DOMContentLoaded", function () {
-    
+
     // 消除选择(单击任意地方3秒后，清除选择)
     // (done)
     var clickTimeout;
@@ -17,26 +17,26 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         }, 3000); // 3s
     });
-    
+
     // 顶部的folder点击，切换显示状态
     // (done)
     var folderMenuHeaderBtn = document.getElementById('folder-menu-header');
-    folderMenuHeaderBtn.addEventListener('click', function(){
+    folderMenuHeaderBtn.addEventListener('click', function () {
         // 旋转图片
         var folderImg = folderMenuHeaderBtn.querySelector('img');
         var imgTransform = window.getComputedStyle(folderImg).getPropertyValue('transform');
-        if(imgTransform == 'none'){
+        if (imgTransform == 'none') {
             folderImg.style.transform = 'rotate(-90deg)';
-        }else{
+        } else {
             folderImg.style.transform = 'none';
         }
         // 隐藏元素
         var menuHeader = document.getElementById('menu-header');
         var hideableElements = menuHeader.querySelectorAll('.hide-able');
-        hideableElements.forEach(function(element){
+        hideableElements.forEach(function (element) {
             element.classList.toggle('hide');
         });
-        
+
     });
 
     // sidebar(左边栏)的folder(底部的folder)点击，切换折叠状态 
@@ -44,17 +44,17 @@ document.addEventListener("DOMContentLoaded", function () {
     // (done)
     var sidebar = document.getElementById('sidebar');
     var folderMenuSidebarBottom = document.getElementById('folder-menu-sidebarBottom');
-    folderMenuSidebarBottom.addEventListener('click', function(){
+    folderMenuSidebarBottom.addEventListener('click', function () {
         // 隐藏元素
         var hideableElements = sidebar.querySelectorAll('.hide-able');
-        hideableElements.forEach(function(element){
+        hideableElements.forEach(function (element) {
             element.classList.toggle('hide');
         });
         // 调整间距
-        
+
 
         var resizableElements = sidebar.querySelectorAll('.resizable');
-        resizableElements.forEach(function(element){
+        resizableElements.forEach(function (element) {
             if (element.classList.contains('mx-10')) {
                 element.classList.remove('mx-10');
                 element.classList.add('re-mx-10');
@@ -62,40 +62,40 @@ document.addEventListener("DOMContentLoaded", function () {
                 // 但还是会收缩（折叠时最宽是66px，但不知为何会伸缩）
                 // 遂，还改为66px，给`#sidebar-top`添加css，min-width: 66px
                 sidebar.style.width = '66px';
-            }else if (element.classList.contains('re-mx-10')) {
+            } else if (element.classList.contains('re-mx-10')) {
                 element.classList.remove('re-mx-10');
                 element.classList.add('mx-10');
                 sidebar.style.width = '16.666667%';
             }
 
-            if (element.classList.contains('mx-8')){
+            if (element.classList.contains('mx-8')) {
                 element.classList.remove('mx-8');
                 element.classList.add('re-mx-8');
-            }else if (element.classList.contains('re-mx-8')){
+            } else if (element.classList.contains('re-mx-8')) {
                 element.classList.remove('re-mx-8');
                 element.classList.add('mx-8');
             }
 
-            if (element.classList.contains('ml-10')){
+            if (element.classList.contains('ml-10')) {
                 element.classList.remove('ml-10');
                 element.classList.add('re-ml-10');
                 element.classList.add('ml-2');
-            }else if (element.classList.contains('re-ml-10')){
+            } else if (element.classList.contains('re-ml-10')) {
                 element.classList.remove('re-ml-10');
                 element.classList.remove('ml-2');
                 element.classList.add('ml-10');
             }
 
-            if (element.classList.contains('p-4')){
+            if (element.classList.contains('p-4')) {
                 element.classList.remove('p-4');
                 element.classList.add('p-2');
                 element.classList.add('re-p-4');
-            }else if (element.classList.contains('re-p-4')){
+            } else if (element.classList.contains('re-p-4')) {
                 element.classList.remove('re-p-4');
                 element.classList.remove('p-2');
                 element.classList.add('p-4');
             }
-            
+
         });
 
     });
@@ -103,23 +103,23 @@ document.addEventListener("DOMContentLoaded", function () {
     // sidebar的"功能"按钮点击，切换折叠状态
     // (done)
     var functionSidebarBtn = document.getElementById('function-sidebar');
-    functionSidebarBtn.addEventListener('click', function(){
+    functionSidebarBtn.addEventListener('click', function () {
         // 旋转图片
         var folderBtn = document.getElementById('folder-function-sidebar');
         var folderImg = folderBtn.querySelector('img');
         var imgTransform = window.getComputedStyle(folderImg).getPropertyValue('transform');
-        if(imgTransform == 'none'){
+        if (imgTransform == 'none') {
             folderImg.style.transform = 'rotate(90deg)';
-        }else{
+        } else {
             folderImg.style.transform = 'none';
         }
         // 隐藏元素
         var functionsSidebarBtn = document.getElementById('functions-sidebar');
         var hideableElements = functionsSidebarBtn.querySelectorAll('.foldable');
-        hideableElements.forEach(function(element){
+        hideableElements.forEach(function (element) {
             element.classList.toggle('hide');
         });
-        
+
     });
 
     // 切换主题模式
@@ -134,7 +134,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    
+
     // sidebar的btns点击效果，以及切换content
     // (unfinished)
     // sidebar已在'侧边栏折叠'中声明
@@ -144,14 +144,14 @@ document.addEventListener("DOMContentLoaded", function () {
     var content = document.getElementById('content');
     var hideableHomepageElements = content.querySelectorAll('.hide-able');
 
-    clickableSidebarElements.forEach(function(element) {
+    clickableSidebarElements.forEach(function (element) {
         // element是each clickable元素
-        element.addEventListener('click', function() {
+        element.addEventListener('click', function () {
             // 切换content(只有这里是"切换content")
             // 获取被点击的元素的 id
             var clickedId = this.id;
             // 遍历所有 hideableElements
-            hideableHomepageElements.forEach(function(hideableElement) {
+            hideableHomepageElements.forEach(function (hideableElement) {
                 // 获取 hideableElement 的 id
                 var hideableId = hideableElement.id;
                 // 如果 hideableElement 的 id 包含 clickedId，则移除 hide 类，否则添加 hide 类
@@ -164,36 +164,36 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
             // 对全部操作
-            clickableSidebarElements.forEach(function(item) {
+            clickableSidebarElements.forEach(function (item) {
                 // item是each clickable元素
                 var textElement = item.querySelector('.text');
                 var imgElement = item.querySelector('img');
                 var originalSrc = imgElement.src;
-                
-                    
+
+
                 // -----改文字-----
                 // (light mode)
                 textElement.classList.remove('text-purple-600');
                 textElement.classList.add('text-zinc-950');
                 // (dark mode)
                 textElement.classList.remove('dark:font-bold');
-                    
+
                 // -----改图片-----
-                
-                if(htmlElement.classList.contains('dark')){
+
+                if (htmlElement.classList.contains('dark')) {
                     // (DARK mode)
                     // 改的是light mode的图片
                     // 因为dark mode懒得做单独的颜色，用css改的
                     // 要切换到light mode时，未选中的颜色改为黑色
-                    if(originalSrc.includes('9013fe')){
+                    if (originalSrc.includes('9013fe')) {
                         imgElement.src = originalSrc.replace('/9013fe/', '/333/');
                     }
-                }else{
+                } else {
                     // (LIGHT mode)
-                    if(originalSrc.includes('9013fe')){
+                    if (originalSrc.includes('9013fe')) {
                         imgElement.src = originalSrc.replace('/9013fe/', '/333/');
                     }
-                    
+
                 }
 
                 // -----改背景、':hover'与':before'-----
@@ -225,19 +225,19 @@ document.addEventListener("DOMContentLoaded", function () {
             this.classList.add('dark:font-bold');
 
             // -----改图片-----
-            
-            if(htmlElement.classList.contains('dark')){
+
+            if (htmlElement.classList.contains('dark')) {
                 // (DARK mode)
-                if(originalSrc.includes('333')){
+                if (originalSrc.includes('333')) {
                     thisImg.src = originalSrc.replace('/333/', '/9013fe/');
                 }
-            }else{
+            } else {
                 // (LIGHT mode)
-                if(originalSrc.includes('333')){
+                if (originalSrc.includes('333')) {
                     thisImg.src = originalSrc.replace('/333/', '/9013fe/');
                 }
             }
-            
+
             // -----改背景颜色-----
             // (light mode)
             this.classList.remove('hover:bg-gray-100');
@@ -268,36 +268,20 @@ document.addEventListener("DOMContentLoaded", function () {
 // *************************
 
 
-// 
+// 首页
 // homepage(首页)
-function updateHomepage(){
+function updateHomepage() {
     // 初始化：
     // 立即更新一次时间
     updateHomepageDateTime();
     // 持续性
-    setInterval(updateHomepageDateTime, 1000);// 每秒钟更新一次时间
-    var homepage = document.getElementById('homepage')
-    var signInDurationCustomInputToggle = document.getElementById('signIn-custom-duration-input-toggle');
-    var inputBoxSignInDuration = document.getElementById('inputBox-signIn-duration');
-    var inputBoxSignInCustomDuration = document.getElementById('inputBox-signIn-custom-duration');
-    signInDurationCustomInputToggle.addEventListener('change', function(){
-        if(signInDurationCustomInputToggle.checked){
-            inputBoxSignInDuration.classList.add('hidden');
-            inputBoxSignInDuration.classList.remove('flex');
-            inputBoxSignInCustomDuration.classList.add('flex');
-            inputBoxSignInCustomDuration.classList.remove('hidden');
+    // 每秒钟更新一次时间
+    setInterval(updateHomepageDateTime, 1000);
 
-        }else{
-            inputBoxSignInDuration.classList.add('flex');
-            inputBoxSignInDuration.classList.remove('hidden');
-            inputBoxSignInCustomDuration.classList.add('hidden');
-            inputBoxSignInCustomDuration.classList.remove('flex');
-        }
-    })
 
 }
 // 更新homepage时间
-function updateHomepageDateTime(){
+function updateHomepageDateTime() {
     var timeHomepage = document.getElementById('time-homepage');
     const currentDateTime = new Date();
     const formattedDateTime = currentDateTime.toLocaleString('zh-CN', {
@@ -311,21 +295,170 @@ function updateHomepageDateTime(){
     timeHomepage.textContent = formattedDateTime; // 将格式化后的日期时间设置为元素的文本内容
 }
 
-// 
+// 签到
 // signIn(签到)
-function updateSignIn(){
-    updateInputTange()
+let signInStateMachine = 0;// 签到状态机 0.签到设置；1.正在签到；2.展现签到结果
+function updateSignIn() {
+    updateInputTangeSignIn();// 更新滑动条显示数字
+    customSignIn();// 自定义项
+    signInStart();// 发起签到
+    // 已将“签到持续中”调到“发起签到”函数中
+    // 已将“签到结束”调到“签到持续中”中
+
 }
-// 更新滑动条
-function updateInputTange(){
+// 更新滑动条显示数字
+function updateInputTangeSignIn() {
+    // 输入后改数字
+    // 初始化
     var signInDurationInput = document.getElementById('signIn-duration-input');
     var signInDurationLabel = document.getElementById('signIn-duration-label');
-    signInDurationInput.addEventListener("input", function(){
-        signInDurationLabel.textContent=signInDurationInput.value;
-    });
+    signInDurationLabel.textContent = signInDurationInput.value;
     var signInFrequencyInput = document.getElementById('signIn-frequency-input');
     var signInFrequencyLabel = document.getElementById('signIn-frequency-label');
-    signInFrequencyInput.addEventListener("input", function(){
-        signInFrequencyLabel.textContent=signInFrequencyInput.value;
+    signInFrequencyLabel.textContent = signInFrequencyInput.value;
+    // 持续
+    signInDurationInput.addEventListener("input", function () {
+        signInDurationLabel.textContent = signInDurationInput.value;
+    });
+    signInFrequencyInput.addEventListener("input", function () {
+        signInFrequencyLabel.textContent = signInFrequencyInput.value;
+    });
+
+
+}
+// 签到-自定义
+function customSignIn() {
+    // 签到时长-自定义
+    var signInDurationCustomInputToggle = document.getElementById('signIn-custom-duration-input-toggle');
+    var inputBoxSignInDuration = document.getElementById('inputBox-signIn-duration');
+    var inputBoxSignInCustomDuration = document.getElementById('inputBox-signIn-custom-duration');
+    signInDurationCustomInputToggle.addEventListener('change', function () {
+        if (signInDurationCustomInputToggle.checked) {
+            inputBoxSignInDuration.classList.add('hidden');
+            inputBoxSignInDuration.classList.remove('flex');
+            inputBoxSignInCustomDuration.classList.add('flex');
+            inputBoxSignInCustomDuration.classList.remove('hidden');
+
+        } else {
+            inputBoxSignInDuration.classList.add('flex');
+            inputBoxSignInDuration.classList.remove('hidden');
+            inputBoxSignInCustomDuration.classList.add('hidden');
+            inputBoxSignInCustomDuration.classList.remove('flex');
+        }
+    });
+    // 签到时长-自定义-标准化输入值
+    var signInCustomDurationInput = document.getElementById('signIn-custom-duration-input');
+    signInCustomDurationInput.addEventListener('blur', function () {
+        let inputValue = parseFloat(signInCustomDurationInput.value);
+        if (isNaN(inputValue)) {
+            inputValue = 5; // 如果输入不是有效数字，设置默认值为 5
+        } else {
+            inputValue = Math.floor(inputValue); // 向下取整
+            inputValue = Math.min(120, Math.max(5, inputValue)); // 限制在 5 到 120 之间
+        }
+        signInCustomDurationInput.value = inputValue;
     });
 }
+// 开始签到(没有初始化，偷懒放在`signInContinuing()`中了)
+function signInStart() {
+    var signInStartBtn = document.getElementById('signIn-start-btn');
+    var signInSelect = document.getElementById('signIn-select');
+    var signInInterface = document.getElementById('signIn-interface');
+    signInStartBtn.addEventListener('click', function () {
+        signInSelect.classList.add('hide');
+        signInInterface.classList.remove('hide');
+        signInStateMachine = 1;
+        signInContinuing()// 签到持续中
+    });
+}
+// 签到中
+// (unfinished)
+function signInContinuing() {
+    var signInSelect = document.getElementById('signIn-select');
+    var signInInterface = document.getElementById('signIn-interface');
+    var signInResult = document.getElementById('signIn-result');
+    
+    if (signInStateMachine == 1) {
+        // 返回
+        var signInRestartBtn = document.getElementById('signIn-restart-btn');
+        signInRestartBtn.addEventListener('click', function () {
+            signInInterface.classList.add('hide');
+            signInSelect.classList.remove('hide');
+            signInStateMachine = 0;
+        });
+        // 前进
+        var signInFinishBtn = document.getElementById('signIn-finish-btn');
+        signInFinishBtn.addEventListener('click',function(){
+            signInInterface.classList.add('hide');
+            signInResult.classList.remove('hide');
+            signInStateMachine = 2;
+        });
+        // 获取网页数据
+        var signInCustomDurationInputToggle = document.getElementById('signIn-custom-duration-input-toggle');
+        var duration = 10 * 60;
+        var frequency = 60;
+        if (signInCustomDurationInputToggle.checked) {
+            var signInCustomDurationInput = document.getElementById('signIn-custom-duration-input');
+            var signInFrequencyInput = document.getElementById('signIn-frequency-input');
+            duration = signInCustomDurationInput.value;
+            frequency = signInFrequencyInput.value;
+        } else {
+            var signInDurationInput = document.getElementById('signIn-duration-input');
+            var signInFrequencyInput = document.getElementById('signIn-frequency-input');
+            duration = signInDurationInput.value * 60;
+            frequency = signInFrequencyInput.value;
+        }
+        // 没有获取学生信息，没有已签到学生数量、学生总数。遂只设置初始数据
+        var totalCount = 50;
+        var signedCount = 0;
+        // 在持续性消息中，缺少获取已签到数量（unfinished）
+
+        // 持续性改变消息
+        // 倒计时
+        var countdownTime = document.getElementById('countdownSignInTime');
+        const now = new Date().getTime();
+        const endTime = now + duration * 1000;
+
+        const interval = setInterval(function () {
+            const currentTime = new Date().getTime();
+            const remainingTime = endTime - currentTime;
+
+            if (remainingTime <= 0) {
+                clearInterval(interval);
+                countdownTime.textContent = '00:00';
+                signInStateMachine = 2;
+            } else {
+                if (signInStateMachine == 0) {// 0状态，销毁计时器
+                    clearInterval(interval);
+                    signInInterface.classList.add('hide');
+                    signInSelect.classList.remove('hide');
+                } else if (signInStateMachine == 1) {
+                    const minutes = Math.floor(remainingTime / 1000 / 60);
+                    const seconds = Math.floor((remainingTime / 1000) % 60);
+                    const formattedTime = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+                    countdownTime.textContent = formattedTime;
+                } else if (signInStateMachine == 2) {
+                    clearInterval(interval);
+                    signInInterface.classList.add('hide');
+                    signInResult.classList.remove('hide');
+                    signInFinished();
+                }
+            }
+        }, 1000);
+    }
+}
+// 签到结束
+// (unfinished) 
+function signInFinished() {
+    var signInSelect = document.getElementById('signIn-select');
+    var signInInterface = document.getElementById('signIn-interface');
+    var signInResult = document.getElementById('signIn-result');
+
+    var signInReturnBtn = document.getElementById('signIn-return-btn');
+
+    signInReturnBtn.addEventListener('click', function(){
+        signInResult.classList.add('hide');
+        signInSelect.classList.remove('hide');
+    });
+}
+
