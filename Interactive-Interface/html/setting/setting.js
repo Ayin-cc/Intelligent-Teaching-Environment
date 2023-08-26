4b95fee830c382307ff5ff4bc3c2b8201b1ec599
XingYueChenFu <128380223+XingYueChenFu@users.noreply.github.com>
2023-08-26 22:32:29 +0800

    Delete Interactive-Interface directory

--- Interactive-Interface/html/setting/setting.js
+++
@@ -1,8 +0,0 @@
-
-
-window.addEventListener('DOMContentLoaded', () => {
-    let btn_closeWindow = document.getElementsByClassName('btn-closeWindow')
-    btn_closeWindow[0].addEventListener('click', function () {
-        window.ipcRenderer.send('settingWindow_close','1')
-    })
-})
