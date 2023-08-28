782ea83e94e47196331c24734bf269f2d3b2188e
Ayin <2752655197@qq.com>
2023-08-28 22:18:55 +0800

    dao层更新，添加 更多的数据库操作

--- src/main/java/dao/QRCodeDao.java
+++ src/main/java/dao/QRCodeDao.java
@@ -1,16 +1,28 @@
 package dao;
 
-import entity.QRcode;
-import entity.Student;
+import entity.*;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface QRCodeDao {
-    int createQRCode(QRcode qRcode);
-    int updateQRCode(String uid, String sid);
-    QRcode selectQRCodeByCid(String cid);
-    QRcode selectQRCodeByUid(String uid);
-    Student selectStudentByToken(String token);
-    int checkCid(String cid);
-    int checkScan(String token, String uid);
+    void createQRCode(@Param("qrcode") QRcode qrcode);
+    void updateQRCode(@Param("uid") String uid, @Param("sid") String sid);
+    List<QRCodeResult> selectQRCodeByMulti(@Param("cid") String cid, @Param("date")  String date, @Param("uid") String uid, @Param("teacher") String teacher, @Param("name") String name, @Param("id") String id);
+    List<QRcode> selectQRCodeByCid(@Param("cid") String cid);
+    List<String> selectStudentByCourseId(@Param("id") String id);
+    Course selectCourseByDate(@Param("cid") String cid, @Param("date") String date, @Param("section") Integer section);
+    List<QRcode> selectQRCodeByDate(@Param("date") String date);
+    List<Course> selectCourseByTeacher(@Param("teacher") String teacher);
+    List<Course> selectCourseByName(@Param("name") String name);
+    List<Course> selectCourseByCourseId(@Param("id") String id);
+    List<Student> selectStudentByUid(@Param("uid") String uid);
+    QRcode selectQRCodeByUid(@Param("uid") String uid);
+    Student selectStudentByToken(@Param("token") String token);
+    int checkCid(@Param("cid") String cid);
+    int checkUid(@Param("uid") String uid);
+    int checkScan(@Param("token") String token, @Param("uid") String uid);
+    int checkStudentInCourse(@Param("token") String token, @Param("courseId") String courseId);
 }
